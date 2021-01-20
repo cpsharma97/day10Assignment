97003aeb1db44c6e3cbb1b17ce7a3cb72872b074
unknown <cpsharma97>
2021-01-21 00:39:09 +0530

    Use case 4

---
+++ uc4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+read -p "Enter first input : " a 
+read -p "Enter second input : " b
+read -p "Enter third input : " c
+
+computeOne=$(( $a + $b * $c ))
+computeTwo=$(( $a * $b + $c ))
+computeThree=$(( $c + $a / $b ))
